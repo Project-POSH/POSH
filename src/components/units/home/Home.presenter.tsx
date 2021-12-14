88e7f0db406f3810138cf582863c4e1edc247b2d
user <filament3@gmail.com>
2021-12-14 19:44:23 +0900

    Fix : eslint fix & 주석 정리

--- src/components/units/home/Home.presenter.tsx
+++ src/components/units/home/Home.presenter.tsx
@@ -3,18 +3,13 @@ import {
   Products,
   ProductItem,
   ProductImgWrapper,
-  CateWrapper,
-  Category,
-  CateDiv,
   ProductPrice,
   ProductImg,
   SelectWrapper,
   CateSelect,
-  SortSelect,
 } from "./Home.styles";
 import InfiniteScroll from "react-infinite-scroller";
 import { useRouter } from "next/router";
-import { sortBy } from "lodash";
 
 export default function HomeUI(props: any) {
   const router = useRouter();
@@ -28,46 +23,8 @@ export default function HomeUI(props: any) {
     console.log(event.target.value);
   }
 
-  //sort 정렬
-  // const items = props.data?.fetchUseditems.map(
-  //   (el: any) => el.price
-  // );
-  // if (Array.isArray(props.data?.fetchUseditems)) {
-  //   const tempOption = props.data?.fetchUseditems.map((item, index) => {
-  //     if (index !== 1) {
-  //       return item;
-  //     }
-  //   });
-  //   const b = tempOption.sort((a, b) => b.price - a.price);
-  //   console.log(b);
-  // }
-  // console.log(typeof props.data?.fetchUseditems[0]);
-
   return (
     <>
-      {/* <CateWrapper>
-        <CateDiv>
-          <Category onClick={onClickCategory} id="top">
-            Top
-          </Category>
-
-          <Category onClick={onClickCategory} id="bottom">
-            Bottom
-          </Category>
-
-          <Category onClick={onClickCategory} id="shoes">
-            Shoes
-          </Category>
-
-          <Category onClick={onClickCategory} id="bag">
-            Bag
-          </Category>
-
-          <Category onClick={onClickCategory} id="acc">
-            Acc
-          </Category>
-        </CateDiv> */}
-      {/* </CateWrapper> */}
       <SelectWrapper>
         <CateSelect onChange={onClickCategory}>
           <option value="home">All ITMES</option>
@@ -77,11 +34,6 @@ export default function HomeUI(props: any) {
           <option value="bag">BAG</option>
           <option value="acc">ACC</option>
         </CateSelect>
-        {/* <SortSelect>
-          <option>최신순</option>
-          <option>낮은가격</option>
-          <option>높은가격</option>
-        </SortSelect> */}
       </SelectWrapper>
       <ContentsWrapper>
         {props.data && (
@@ -115,9 +67,6 @@ export default function HomeUI(props: any) {
             </Products>
           </InfiniteScroll>
         )}
-        {/* <a href="#" style={{ width: "500px", backgroundColor: "blue" }}>
-          위로가기
-        </a> */}
       </ContentsWrapper>
     </>
   );
